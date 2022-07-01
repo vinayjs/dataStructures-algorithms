b5f723e177bd90b882783632fbf9a850b4807925
VINAY CHANDRAN C <100291822+vinayjs@users.noreply.github.com>
2022-07-01 20:40:29 +0530

    Add files via upload

---
+++ prototype.js
@@ -0,0 +1,52 @@
+/*
+const me = {
+    talk() {
+        return "talking";
+
+    }
+}
+
+
+console.log(me.talk());
+
+
+const you = {
+    talk(){
+        return "talking"
+
+    }
+};
+console.log(you.talk());
+
+
+// CLASS--
+class Person {
+    talk(){
+        return "talking";
+    }
+};
+
+const me = new Person();
+const you = new Person();
+
+console.log(me.talk());
+console.log(you.talk());
+
+Person.prototype.talk=()=>{
+    return ' I AM LISTENING'
+};
+console.log(me.talk());
+console.log(you.talk());
+
+
+const person = {
+    talk(){
+        return "talking";
+    }
+};
+
+const me = {};
+Object.setPrototypeOf(me, person);
+console.log(me.talk());
+
+*/
