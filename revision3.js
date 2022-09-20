7eacc545be67229bd2360f3db6ea7b3bbf6d6465
VINAY CHANDRAN C <100291822+vinayjs@users.noreply.github.com>
2022-09-20 20:20:21 +0530

    patterns - algorithms and data structures

--- revision3.js
+++ revision3.js
@@ -759,3 +759,211 @@ const maxSub = (arr, k) => {
 // result = reverseSubList(head, 3, 5);
 // console.log("new order : ");
 // result.printList();
+
+//Remove Duplicates
+// Input: [2, 3, 3, 3, 6, 9, 9]
+// Output: 4
+// Explanation: The first four elements after removing the duplicates will be [2, 3, 6, 9].
+
+// const removeDup = (arr) => {
+//     let nextNonDup = 1;
+//     let i = 0;
+//     while(i < arr.length){
+//         if(arr[nextNonDup - 1] !== arr[i]){
+//             arr[nextNonDup] = arr[i];
+//             nextNonDup += 1;
+//         }
+//         i += 1;
+//     }
+//     return nextNonDup;
+// }
+
+// console.log(removeDup([2, 3, 3, 3, 6, 9, 9]));
+
+// const arr =  [-2, -1, 0, 2, 3]
+// const result = Array(arr.length - 1).fill(0)
+// let left = 0
+// let right = arr.length - 1
+// let lastIndex = arr.length - 1
+
+// while (left < right) {
+//   let leftSquare = arr[left] * arr[left]
+//   let rightSquare = arr[right] * arr[right]
+//   if (leftSquare > rightSquare) {
+//     result[lastIndex] = leftSquare
+//     left ++
+//   } else {
+//     result[lastIndex] = rightSquare
+//     right --
+//   }
+//   lastIndex --
+// }
+// console.log(result)
+
+// Given a sorted array,
+// create a new array containing squares of all the numbers of the input array in the sorted order.
+
+// Input: [-2, -1, 0, 2, 3]
+// Output: [0, 1, 4, 4, 9]
+
+// const makeSquares = (arr) => {
+//   const n = arr.length;
+//   squares = Array(n).fill(0);
+//   let highestSquareIndex = n - 1;
+//   let left = 0;
+//   let right = n - 1;
+//   while (left <= right) {
+//     let leftSquare = arr[left] * arr[left];
+//     let rightSquare = arr[right] * arr[right];
+//     if (leftSquare > rightSquare) {
+//       squares[highestSquareIndex] = leftSquare;
+//       left += 1;
+//     } else {
+//       squares[highestSquareIndex] = rightSquare;
+//       right -= 1;
+//     }
+//     highestSquareIndex -= 1;
+//   }
+//   return squares;
+// };
+// console.log(makeSquares([-2, -1, 0, 2, 3]));
+
+// Given an array of unsorted numbers,
+// find all unique triplets in it that add up to zero.
+// Input: [-3, 0, 1, 2, -1, 1, -2]
+// Output: [-3, 1, 2], [-2, 0, 2], [-2, 1, 1], [-1, 0, 1]
+// Explanation: There are four unique triplets whose sum is equal to zero.
+
+// const searchTriplets = (arr) => {
+//   arr.sort((a, b) => a - b);
+//   const triplets = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (i > 0 && arr[i] === arr[i - 1]) {
+//       continue;
+//     }
+//     searchPair(arr, -arr[i], i + 1, triplets);
+//   }
+//   return triplets;
+// };
+// const searchPair = (arr, targetSum, left, triplets) => {
+//   let right = arr.length - 1;
+//   while (left < right) {
+//     const currentSum = arr[left] + arr[right];
+//     if (currentSum === targetSum) {
+//       triplets.push([-targetSum, arr[left], arr[right]]);
+//       left += 1;
+//       right -= 1;
+//       while (left < right && arr[left] === arr[left - 1]) {
+//         left += 1;
+//       }
+//       while (left < right && arr[right] === arr[right + 1]) {
+//         right -= 1;
+//       }
+//     } else if (targetSum > currentSum) {
+//       left += 1;
+//     } else {
+//       right -= 1;
+//     }
+//   }
+// };
+// console.log(searchTriplets([-3, 0, 1, 2, -1, 1, -2]));
+
+// Given an array of unsorted numbers and a target number,
+// find a triplet in the array whose sum is as close to the target number as possible,
+// return the sum of the triplet. If there are more than one such triplet,
+// return the sum of the triplet with the smallest sum.
+
+// Input: [-2, 0, 1, 2], target=2
+// Output: 1
+// Explanation: The triplet [-2, 1, 2] has the closest sum to the target.
+
+// const tripletSum = (arr, targetSum) => {
+//     arr.sort((a, b) => a - b);
+//     let smallDifferenece = Infinity;
+//     for (let i = 0; i < arr.length - 2; i++) {
+//         let left = i + 1;
+//         let right = arr.length - 1;
+//         while (left < right) {
+//             const targetDifference = targetSum - arr[i] - arr[left] - arr[right];
+//             if (targetDifference === 0) {
+//                 return targetSum
+//             }
+//             if (Math.abs(targetDifference) < Math.abs(smallDifferenece) ||
+//                (Math.abs(targetDifference) === Math.abs(smallDifferenece) && targetDifference > smallDifferenece)) {
+//                 smallDifferenece = targetDifference;
+//             }
+//             if (targetDifference > 0) {
+//                 left += 1;
+//             } else {
+//               right -= 1;
+//             }
+//         }
+//     }
+//     return targetSum - smallDifferenece;
+// }
+// console.log(tripletSum([-2, 0, 1, 2], 2));
+// // Explanation: The longest substring with no more than '2' distinct characters is "araa".
+
+// const subString = (str, k) => {
+//   let windowStart = 0,
+//   maxLength = 0,
+//   charFrequency = {};
+// // in the following loop we'll try to extend the range [window_start, window_end]
+// for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+//   const rightChar = str[windowEnd];
+//   if (!(rightChar in charFrequency)) {
+//     charFrequency[rightChar] = 0;
+//   }
+//   charFrequency[rightChar] += 1;
+//   // shrink the sliding window, until we are left with 'k' distinct characters in the char_frequency
+//   while (Object.keys(charFrequency).length > k) {
+//     const leftChar = str[windowStart];
+//     charFrequency[leftChar] -= 1;
+//     if (charFrequency[leftChar] === 0) {
+//       delete charFrequency[leftChar];
+//     }
+//     windowStart += 1; // shrink the window
+//   }
+//   // remember the maximum length so far
+//   maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+// }
+
+// return maxLength;
+// }
+
+// console.log(subString("araaci", 2));
+// =============================================================================================================================
+
+// // Input: Fruit=['A', 'B', 'C', 'A', 'C']
+// // Output: 3
+// // Explanation: We can put 2 'C' in one basket and one 'A' in the other from the subarray ['C', 'A', 'C']
+
+// const fruits_into_baskets = (fruits) => {
+//   let windowStart = 0,
+//     maxLength = 0,
+//     fruitFrequency = {};
+
+//   // try to extend the range [windowStart, windowEnd]
+//   for (let windowEnd = 0; windowEnd < fruits.length; windowEnd++) {
+//     const rightFruit = fruits[windowEnd];
+//     if (!(rightFruit in fruitFrequency)) {
+//       fruitFrequency[rightFruit] = 0;
+//     }
+//     fruitFrequency[rightFruit] += 1;
+
+//     // shrink the sliding window, until we are left with '2' fruits in the fruit frequency dictionary
+//     while (Object.keys(fruitFrequency).length > 2) {
+//       const leftFruit = fruits[windowStart];
+//       fruitFrequency[leftFruit] -= 1;
+//       if (fruitFrequency[leftFruit] === 0) {
+//         delete fruitFrequency[leftFruit];
+//       }
+//       windowStart += 1; // shrink the window
+//     }
+//     maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+//   }
+
+//   return maxLength;
+// }
+
+// console.log(fruits_into_baskets(['A', 'B', 'C', 'A', 'C']));
