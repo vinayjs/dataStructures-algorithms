5c727c2794e3701920efc0cde4ce2855529de062
VINAY CHANDRAN C <100291822+vinayjs@users.noreply.github.com>
2022-10-05 23:01:32 +0530

    Patterns Data Structures and Algorithms

--- revision3.js
+++ revision3.js
@@ -5057,23 +5057,24 @@
 // Explanation: The longest substring with distinct characters is "ab".
 
 // Input: String="abbbb"
-// Output: 2
-// Explanation: The longest substring with distinct characters is "ab".
+// // Output: 2
+// // Explanation: The longest substring with distinct characters is "ab".
 
-const longSubString = (str) => {
-    let windowStart = 0;
-    let maxLength = 0;
-    charIndexMap = {};
+// const longSubString = (str) => {
+//     let windowStart = 0;
+//     let maxLength = 0;
+//     charIndexMap = {};
 
-    for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
-        const rightChar = str[windowEnd];
-        if (rightChar in charIndexMap) {
-            windowStart = Math.max(windowStart, charIndexMap[rightChar] + 1)
-        }
-        charIndexMap[rightChar] = windowEnd;
-        maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
-    }
-    return maxLength;
-}
-console.log(longSubString("abacbd"))
+//     for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+//         const rightChar = str[windowEnd];
+//         if (rightChar in charIndexMap) {
+//             windowStart = Math.max(windowStart, charIndexMap[rightChar] + 1)
+//         }
+//         charIndexMap[rightChar] = windowEnd;
+//         maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+//     }
+//     return maxLength;
+// }
+// console.log(longSubString("abacbd"))
+
 
