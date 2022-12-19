78ccb28383f3c8a67d28830952056202d259299f
VINAY CHANDRAN C <vinay.sarvodaya1991@gmail.com>
2022-12-19 20:58:36 +0530

    patterns -merge sort

--- revision.js
+++ revision.js
@@ -388,3 +388,52 @@ const targetSum = (arr,target) => {
 }
 
 console.log(targetSum([1, 2, 3, 4, 6], 6));
+
+// Given a list of intervals,
+// merge all the overlapping intervals to produce
+// a list that has only mutually exclusive intervals.
+
+// Intervals: [[1,4], [2,5], [7,9]]
+// Output: [[1,5], [7,9]]
+// Explanation: Since the first two intervals [1,4] and [2,5] overlap, we merged them into
+// one [1,5].
+
+class Interval {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  getInterval() {
+    return "[" + this.start + ", " + this.end + "]";
+  }
+}
+
+const merge = (intervals) => {
+  if (intervals.length < 2) {
+    return intervals;
+  }
+  intervals.sort((a, b) => a.start - b.start);
+  const mergedIntervals = [];
+  let start = intervals[0].start,
+    end = intervals[0].end;
+  for (i = 1; i < intervals.length; i++) {
+    const interval = intervals[i];
+    if (interval.start <= end) {
+      end = Math.max(interval.end, end);
+    } else {
+      mergedIntervals.push(new Interval(start, end));
+      start = interval.start;
+      end = interval.end;
+    }
+  }
+  mergedIntervals.push(new Interval(start, end));
+  return mergedIntervals;
+};
+
+let result = merge([
+  new Interval(1, 4),
+  new Interval(2, 5),
+  new Interval(7, 9),
+]);
+console.log("Merged intervals :", result);
