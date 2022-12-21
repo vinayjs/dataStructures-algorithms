bd5eeee7439a1746e9a096d55e781fc35994c6fb
VINAY CHANDRAN C <vinay.sarvodaya1991@gmail.com>
2022-12-21 21:36:00 +0530

    patterns- bfs

--- revision.js
+++ revision.js
@@ -33,6 +33,118 @@
 // //         }
 // //         console.log(result);
 // //     }
+
+
+// Given a binary tree, populate an array to represent its level-by-level traversal in reverse order, i.e., the lowest level comes first.
+// You should populate the values of all nodes in each level from left to right in separate sub-arrays.
+
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
+const traverse = (root) => {
+  result = [];
+  if (root === null) {
+    return result
+  }
+  const queue = []
+  queue.push(root)
+  while(queue.length > 0){
+    const levelSize = queue.length;
+    currentLevel = [];
+    for (let i = 0; i < levelSize; i++) {
+        currentNode = queue.shift();
+        currentLevel.push(currentNode.val)
+
+        if(currentNode.left !== null) {
+            queue.push(currentNode.left)
+        }
+        if(currentNode.right !== null) {
+            queue.push(currentNode.right)
+        }
+    }
+    result.unshift(currentLevel);
+  }
+  return result;
+};
+
+const root = new TreeNode(12);
+root.left = new TreeNode(7);
+root.right = new TreeNode(1);
+root.left.left = new TreeNode(9);
+root.right.left = new TreeNode(10);
+root.right.right = new TreeNode(5);
+console.log(`Level order traversal: ${traverse(root)}`);
+
+
+// Given a binary tree, populate an array to represent its zigzag level order 
+// traversal. 
+// from left to right then from right to left.
+
+
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
+
+const traverse = (root) => {
+  let result = [];
+  if (root === null) {
+    return result
+  }
+  let queue = [];
+  queue.push(root);
+  leftToRight = true;
+  while(queue.length > 0) {
+    const levelSize = queue.length;
+    let currentLevel = []
+    for (let i = 0; i < levelSize; i++) {
+      currentNode = queue.shift();
+      if(leftToRight) {
+        currentLevel.push(currentNode.val)
+      }else {
+        currentLevel.unshift(currentNode.val)
+      }
+      if(currentNode.left !== null) {
+        queue.push(currentNode.left)
+      }
+      if(currentNode.right !== null) {
+        queue.push(currentNode.right)
+      }
+    }
+    result.push(currentLevel)
+    leftToRight = !leftToRight
+  }
+  
+  return result;
+};
+
+
+let root = new TreeNode(12)
+root.left = new TreeNode(7)
+root.right = new TreeNode(1)
+root.left.left = new TreeNode(9)
+root.right.left = new TreeNode(10)
+root.right.right = new TreeNode(5)
+root.right.left.left = new TreeNode(20)
+root.right.left.right = new TreeNode(17)
+console.log(`Zigzag traversal: ${traverse(root)}`)
+
+
+
+
+
+
 // // }
 
 // // num(5);
