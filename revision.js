4ceb7e4f7e5514585f73dbc3ee7179a64c71d4e6
VINAY CHANDRAN C <vinay.sarvodaya1991@gmail.com>
2022-12-22 21:47:43 +0530

    patterns-dfs- data structures and algorithms

--- revision.js
+++ revision.js
@@ -549,3 +549,40 @@ let result = merge([
   new Interval(7, 9),
 ]);
 console.log("Merged intervals :", result);
+
+
+// DFS
+// Given a binary tree and a number ‘S’, 
+// find if the tree has a path from root-to-leaf such that the sum of all the node values of that path equals ‘S’.
+
+// class TreeNode {
+
+//     constructor(val) {
+//       this.val = val;
+//       this.left = null;
+//       this.right = null; 
+//     }
+//   };
+  
+  
+//   const hasPath = (root, sum) => {
+//     if (root === null) {
+//         return false;
+//     }
+    
+//     if (root.val === sum && root.left === null && root.right === null) {
+//         return true
+//     }
+
+//     return hasPath(root.left, sum - root.val) || hasPath(root.right, sum - root.val)
+//   };
+  
+  
+//   var root = new TreeNode(12)
+//   root.left = new TreeNode(7)
+//   root.right = new TreeNode(1)
+//   root.left.left = new TreeNode(9)
+//   root.right.left = new TreeNode(10)
+//   root.right.right = new TreeNode(5)
+//   console.log(`Tree has path: ${hasPath(root, 23)}`)
+//   console.log(`Tree has path: ${hasPath(root, 16)}`)
